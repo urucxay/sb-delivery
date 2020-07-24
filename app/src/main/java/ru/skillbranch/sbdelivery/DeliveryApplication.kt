92bcecd83d687a98c1bcae8ae2a4561d0f1920cf
UrucXay <urucxay@gmail.com>
2020-07-25 00:04:16 +0300

    add Application class for Koin and Timber

---
+++ app/src/main/java/ru/skillbranch/sbdelivery/DeliveryApplication.kt
@@ -0,0 +1,30 @@
+package ru.skillbranch.sbdelivery
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import ru.skillbranch.sbdelivery.di.modulesList
+import timber.log.Timber
+
+class DeliveryApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initLogger()
+        initKoin()
+    }
+
+    private fun initLogger() {
+        Timber.plant(Timber.DebugTree())
+    }
+
+    private fun initKoin() {
+        startKoin {
+            androidLogger()
+            androidContext(this@DeliveryApplication)
+            modules(modulesList)
+        }
+    }
+
+}
