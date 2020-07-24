11bb20a797db9dd1a295cc59988cbf984dc56317
UrucXay <urucxay@gmail.com>
2020-07-25 00:05:39 +0300

    add modules for koin

---
+++ app/src/main/java/ru/skillbranch/sbdelivery/di/Modules.kt
@@ -0,0 +1,9 @@
+package ru.skillbranch.sbdelivery.di
+
+import ru.skillbranch.sbdelivery.di.modules.networkModule
+import ru.skillbranch.sbdelivery.di.modules.viewModelsModule
+
+val modulesList = listOf(
+    networkModule,
+    viewModelsModule
+)
