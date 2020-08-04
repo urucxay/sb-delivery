d5008f47f4b6ef3542504d7085d27b60cd0416e8
UrucXay <urucxay@gmail.com>
2020-08-04 14:11:58 +0300

    fix typo

--- app/src/main/java/ru/skillbranch/sbdelivery/ui/custom/HorizontalSpacingItemDecorator.kt
+++ app/src/main/java/ru/skillbranch/sbdelivery/ui/custom/HorizontalSpacingItemDecorator.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import ru.skillbranch.sbdelivery.util.dpToIntPx
 
-class HorisontalSpacingItemDecorator<in T : ListAdapter<D, VH>, D, VH : RecyclerView.ViewHolder>(
+class HorizontalSpacingItemDecorator<in T : ListAdapter<D, VH>, D, VH : RecyclerView.ViewHolder>(
     private val adapter: T,
     private val right: Int = 8
 ) : RecyclerView.ItemDecoration() {
