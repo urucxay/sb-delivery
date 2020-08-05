6651b33b6cf13e1d8b93679a927584dbeee4318b
UrucXay <urucxay@gmail.com>
2020-08-05 21:23:45 +0300

    add RootViewModel, DishViewModel

--- app/src/main/java/ru/skillbranch/sbdelivery/di/modules/ViewModelsModule.kt
+++ app/src/main/java/ru/skillbranch/sbdelivery/di/modules/ViewModelsModule.kt
@@ -1,13 +1,16 @@
 package ru.skillbranch.sbdelivery.di.modules
 
+import androidx.lifecycle.SavedStateHandle
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-import ru.skillbranch.sbdelivery.ui.home.HomeViewModel
+import ru.skillbranch.sbdelivery.ui.activity.root.RootViewModel
+import ru.skillbranch.sbdelivery.ui.fragment.dish.DishViewModel
+import ru.skillbranch.sbdelivery.ui.fragment.home.HomeViewModel
 
 val viewModelsModule = module {
 
-    viewModel {
-        HomeViewModel()
-    }
+    viewModel { (handle: SavedStateHandle) -> RootViewModel(handle) }
+    viewModel { (handle: SavedStateHandle) -> HomeViewModel(handle, get()) }
+    viewModel { (handle: SavedStateHandle, dishId: String) -> DishViewModel(handle, dishId, get()) }
 
 }
