b415b686382a838a78b556aef9e1032a76cfd031
UrucXay <urucxay@gmail.com>
2020-08-16 18:14:00 +0300

    Add new listeners for favoriteButton and addToCartButton

--- app/src/main/java/ru/skillbranch/sbdelivery/ui/adapter/DishAdapter.kt
+++ app/src/main/java/ru/skillbranch/sbdelivery/ui/adapter/DishAdapter.kt
@@ -10,9 +10,16 @@ import com.bumptech.glide.Glide
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.dish_item_view.*
 import ru.skillbranch.sbdelivery.R
-import ru.skillbranch.sbdelivery.network.response.DishResponse
+import ru.skillbranch.sbdelivery.db.entitiy.DishItem
+import ru.skillbranch.sbdelivery.util.hide
+import ru.skillbranch.sbdelivery.util.setThrottledClickListener
+import ru.skillbranch.sbdelivery.util.show
 
-class DishAdapter(private val listener: (DishResponse) -> Unit) : ListAdapter<DishResponse, DishAdapter.ViewHolder>(DishItemCallback) {
+class DishAdapter(
+    private val listener: (DishItem) -> Unit,
+    private val favoriteListener: (DishItem) -> Unit,
+    private val addToCartListener: (DishItem) -> Unit
+) : ListAdapter<DishItem, DishAdapter.ViewHolder>(DishItemCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -21,29 +28,47 @@ class DishAdapter(private val listener: (DishResponse) -> Unit) : ListAdapter<Di
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(currentList[position], listener)
+        holder.bind(currentList[position], listener, favoriteListener, addToCartListener)
     }
 
     class ViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView),
         LayoutContainer {
 
-        fun bind(dish: DishResponse, listener: (DishResponse) -> Unit) {
+        fun bind(
+            dish: DishItem,
+            listener: (DishItem) -> Unit,
+            favoriteListener: (DishItem) -> Unit,
+            addToCartListener: (DishItem) -> Unit
+        ) {
             tvName.text = dish.name
             tvPrice.text = tvPrice.context.getString(R.string.price, dish.price.toString())
+            btnFavorite.isChecked = dish.isFavorite
+            if (dish.oldPrice != null) {
+                groupPromo.show()
+            } else {
+                groupPromo.hide()
+            }
             Glide.with(ivDish.context)
                 .load(dish.image)
                 .into(ivDish)
-            containerView.setOnClickListener {
+
+            containerView.setThrottledClickListener {
                 listener(dish)
+            }
+            btnFavorite.setThrottledClickListener {
+                favoriteListener(dish)
+            }
+            btnAddToCart.setThrottledClickListener {
+                addToCartListener(dish)
             }
         }
     }
 }
 
-object DishItemCallback : DiffUtil.ItemCallback<DishResponse>() {
-    override fun areItemsTheSame(oldItem: DishResponse, newItem: DishResponse) =
+object DishItemCallback : DiffUtil.ItemCallback<DishItem>() {
+    override fun areItemsTheSame(oldItem: DishItem, newItem: DishItem) =
         oldItem.id == newItem.id
 
-    override fun areContentsTheSame(oldItem: DishResponse, newItem: DishResponse) =
+    override fun areContentsTheSame(oldItem: DishItem, newItem: DishItem) =
         oldItem == newItem
 }
