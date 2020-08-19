fe6f780755d1adc908792bdfca946180c70a8c35
UrucXay <urucxay@gmail.com>
2020-08-19 19:01:20 +0300

    Add temporary fix for koin for kotlin 1.4

--- app/src/main/java/ru/skillbranch/sbdelivery/DeliveryApplication.kt
+++ app/src/main/java/ru/skillbranch/sbdelivery/DeliveryApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import ru.skillbranch.sbdelivery.di.modulesList
 import timber.log.Timber
 
@@ -21,7 +22,8 @@ class DeliveryApplication : Application() {
 
     private fun initKoin() {
         startKoin {
-            androidLogger()
+            //Temporary fix for Kotlin 1.4
+            androidLogger(Level.ERROR)
             androidContext(this@DeliveryApplication)
             modules(modulesList)
         }
