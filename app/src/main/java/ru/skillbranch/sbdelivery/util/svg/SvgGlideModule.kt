283fc9d5d6f3feac61ac44e9f849a27ba21d07d1
UrucXay <urucxay@gmail.com>
2020-08-19 18:54:05 +0300

    Add SVG decoder for Glide
    This will be needed to set the category icon in MenuFragment.

---
+++ app/src/main/java/ru/skillbranch/sbdelivery/util/svg/SvgGlideModule.kt
@@ -0,0 +1,17 @@
+package ru.skillbranch.sbdelivery.util.svg
+
+import android.content.Context
+import android.util.Log
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class SvgGlideModule : AppGlideModule() {
+    override fun isManifestParsingEnabled() = false
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+        builder.setLogLevel(Log.DEBUG)
+    }
+}
