10ac0453ea7c510a2b82856c7f62816986235f29
UrucXay <urucxay@gmail.com>
2020-08-20 18:17:01 +0300

    Add custom layout manager for menu screen

---
+++ app/src/main/java/ru/skillbranch/sbdelivery/ui/adapter/AutoFitGridLayoutManager.kt
@@ -0,0 +1,41 @@
+package ru.skillbranch.sbdelivery.ui.adapter
+
+import android.content.Context
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import kotlin.math.max
+
+
+class AutoFitGridLayoutManager(context: Context?, columnWidth: Int) :
+    GridLayoutManager(context, 1) {
+    private var columnWidth = 0
+    private var columnWidthChanged = true
+
+    init {
+        setColumnWidth(columnWidth)
+    }
+
+    private fun setColumnWidth(newColumnWidth: Int) {
+        if (newColumnWidth > 0 && newColumnWidth != columnWidth) {
+            columnWidth = newColumnWidth
+            columnWidthChanged = true
+        }
+    }
+
+    override fun onLayoutChildren(recycler: Recycler, state: RecyclerView.State) {
+        if (columnWidthChanged && columnWidth > 0) {
+            val totalSpace: Int = if (orientation == VERTICAL) {
+                width - paddingRight - paddingLeft
+            } else {
+                height - paddingTop - paddingBottom
+            }
+            val spanCount = max(1, totalSpace / columnWidth)
+            setSpanCount(spanCount)
+            columnWidthChanged = false
+        }
+        super.onLayoutChildren(recycler, state)
+    }
+
+
+}
