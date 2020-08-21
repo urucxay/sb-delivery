1781c97a7891ea1d3dfe14a49a296e42c1ba04be
UrucXay <urucxay@gmail.com>
2020-08-22 00:48:31 +0300

    Make dish screen

--- app/src/main/java/ru/skillbranch/sbdelivery/ui/fragment/dish/DishFragment.kt
+++ app/src/main/java/ru/skillbranch/sbdelivery/ui/fragment/dish/DishFragment.kt
@@ -1,47 +1,104 @@
 package ru.skillbranch.sbdelivery.ui.fragment.dish
 
-import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+import android.text.Spanned
+import android.text.style.StrikethroughSpan
+import androidx.core.text.toSpannable
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_dish.*
 import org.koin.androidx.viewmodel.ext.android.stateViewModel
 import org.koin.core.parameter.parametersOf
 import ru.skillbranch.sbdelivery.R
-import ru.skillbranch.sbdelivery.network.response.DishResponse
-import ru.skillbranch.sbdelivery.ui.adapter.DishAdapter
 import ru.skillbranch.sbdelivery.ui.adapter.ReviewAdapter
 import ru.skillbranch.sbdelivery.ui.base.BaseFragment
+import ru.skillbranch.sbdelivery.ui.base.Binding
+import ru.skillbranch.sbdelivery.ui.base.IViewModelState
+import ru.skillbranch.sbdelivery.ui.base.ToolbarBuilder
 import ru.skillbranch.sbdelivery.ui.custom.OffsetItemDecorator
+import ru.skillbranch.sbdelivery.ui.custom.RenderProp
 import ru.skillbranch.sbdelivery.ui.fragment.dialogs.ReviewDialogFragment
+import ru.skillbranch.sbdelivery.util.setThrottledClickListener
+import ru.skillbranch.sbdelivery.util.show
 
 class DishFragment : BaseFragment<DishViewModel>() {
 
     override val layout = R.layout.fragment_dish
     override val viewModel: DishViewModel by stateViewModel { parametersOf(args.id) }
+    override val binding: Binding = DishBinding()
 
     private val adapter = ReviewAdapter()
     private val args: DishFragmentArgs by navArgs()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
+    override val prepareToolbar: (ToolbarBuilder.() -> Unit) = {
+        this.setTitle(args.name)
     }
 
     override fun setupViews() {
-        btnReviewAdd.setOnClickListener {
-            ReviewDialogFragment().show(childFragmentManager, "dialog")
+        //lock drawer layout menu
+        root.lockDrawerLayout(true)
+
+        tvName.text = args.name
+        tvDescription.text = args.description
+
+        btnReviewAdd.setThrottledClickListener {
+            ReviewDialogFragment().show(childFragmentManager, "review_dialog")
         }
 
-        viewModel.reviews.observe(viewLifecycleOwner, Observer { adapter.submitList(it) })
+        btnFavorite.setThrottledClickListener {
+            viewModel.handleToggleFavorite()
+        }
+
+        btnAddToCart.setThrottledClickListener {
+            viewModel.handleAddToCart(tvNumPicker.count)
+        }
 
         rvReviews.adapter = adapter
         rvReviews.layoutManager =
             LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
         rvReviews.addItemDecoration(OffsetItemDecorator(top = 10))
+
+        viewModel.reviewsLiveData.observe(viewLifecycleOwner) { adapter.submitList(it) }
     }
 
+    inner class DishBinding : Binding() {
+
+        private var oldPrice: String by RenderProp("") {
+            val string = resources.getString(R.string.price, it)
+            val strikethroughPrice = string.toSpannable().apply {
+                setSpan(
+                    StrikethroughSpan(),
+                    0,
+                    string.length,
+                    Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
+            if (it.isNotEmpty()) {
+                tvOldPrice.show()
+                groupPromo.show()
+                tvOldPrice.text = strikethroughPrice
+            }
+        }
+        private var price: Int by RenderProp(0) {
+            tvPrice.text = resources.getString(R.string.price, it.toString())
+        }
+        private var rating: Float by RenderProp(0F) { tvRating.text = it.toString() }
+        private var isFavorite: Boolean by RenderProp(false) { btnFavorite.isChecked = it }
+        private var image: String by RenderProp("") {
+            Glide.with(requireContext())
+                .load(it)
+                .into(ivDish)
+        }
+
+        override fun bind(data: IViewModelState) {
+            data as DishState
+            isFavorite = data.isFavorite
+            price = data.price
+            rating = data.rating
+            oldPrice = data.oldPrice ?: ""
+            image = data.image ?: ""
+        }
+
+    }
 
 }
--- app/src/main/java/ru/skillbranch/sbdelivery/ui/fragment/dish/DishViewModel.kt
+++ app/src/main/java/ru/skillbranch/sbdelivery/ui/fragment/dish/DishViewModel.kt
@@ -1,24 +1,77 @@
 package ru.skillbranch.sbdelivery.ui.fragment.dish
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.liveData
-import ru.skillbranch.sbdelivery.repository.DeliveryRepository
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import ru.skillbranch.sbdelivery.repository.DishRepository
 import ru.skillbranch.sbdelivery.ui.base.BaseViewModel
 import ru.skillbranch.sbdelivery.ui.base.IViewModelState
+import ru.skillbranch.sbdelivery.ui.base.Notification
 import timber.log.Timber
 
 class DishViewModel(
     handle: SavedStateHandle,
-    dishId: String,
-    private val repository: DeliveryRepository
+    private val dishId: String,
+    private val dishRepository: DishRepository
 ) : BaseViewModel<DishState>(handle, DishState()) {
 
-    val reviews = liveData {
-        val list = repository.getReviews(dishId)
-        Timber.d("list -> $list")
-        emit(list)
+    val reviewsLiveData = liveData {
+        dishRepository.getReviews(
+            dishId = dishId,
+            onSuccess = {
+                //TODO change loading status
+            },
+            onError = {
+                //TODO show notification
+            }
+        ).collect { emit(it) }
     }
+
+    init {
+        viewModelScope.launch {
+            val data = dishRepository.getDishById(dishId)
+                .asLiveData()
+            Timber.d("date -> ${data.value}")
+            subscribeOnDataSource(data) { dish, currentState ->
+                currentState.copy(
+                    rating = dish.rating,
+                    price = dish.price,
+                    oldPrice = dish.oldPrice,
+                    image = dish.image,
+                    isFavorite = dish.isFavorite
+                )
+            }
+        }
+    }
+
+    fun handleToggleFavorite() {
+        viewModelScope.launch() {
+            dishRepository.toggleFavorite(dishId)
+        }
+    }
+
+    fun handleAddToCart(count: Int) {
+        notify(Notification.TextMessage("was added to the cart"))
+        dishRepository.addToCart(dishId, count)
+    }
+
+    fun handleAddReview() {
+
+    }
+
 
 }
 
-class DishState : IViewModelState
+data class DishState(
+    val isFavorite: Boolean = false,
+    val count: Int = 1,
+    val rating: Float = 0F,
+    val price: Int = 0,
+    val oldPrice: String? = null,
+    val image: String? = null
+) : IViewModelState {
+
+}
