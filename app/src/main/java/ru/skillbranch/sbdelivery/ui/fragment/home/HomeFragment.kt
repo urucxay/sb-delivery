a027f5b59623b6d53d84c9b65841b5c25048c976
UrucXay <urucxay@gmail.com>
2020-08-22 00:47:29 +0300

    Make home screen

--- app/src/main/java/ru/skillbranch/sbdelivery/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/ru/skillbranch/sbdelivery/ui/fragment/home/HomeFragment.kt
@@ -1,50 +1,119 @@
 package ru.skillbranch.sbdelivery.ui.fragment.home
 
 import android.os.Bundle
-import android.view.View
-import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_home.*
 import org.koin.androidx.viewmodel.ext.android.stateViewModel
 import ru.skillbranch.sbdelivery.R
 import ru.skillbranch.sbdelivery.ui.adapter.DishAdapter
 import ru.skillbranch.sbdelivery.ui.base.BaseFragment
+import ru.skillbranch.sbdelivery.ui.base.MenuItemHolder
+import ru.skillbranch.sbdelivery.ui.base.NavigationCommand
+import ru.skillbranch.sbdelivery.ui.base.ToolbarBuilder
 import ru.skillbranch.sbdelivery.ui.custom.OffsetItemDecorator
+import ru.skillbranch.sbdelivery.util.setThrottledClickListener
+import ru.skillbranch.sbdelivery.util.show
+import timber.log.Timber
 
 class HomeFragment : BaseFragment<HomeViewModel>() {
 
     override val viewModel: HomeViewModel by stateViewModel()
     override val layout: Int = R.layout.fragment_home
 
-    //    private val recommendAdapter = DishAdapter()
-    private val bestAdapter = DishAdapter() {
-        val action = HomeFragmentDirections.actionHomeDestinationToDishFragment(it.id)
-        findNavController().navigate(action)
-    }
-//    private val popularAdapter = DishAdapter()
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        viewModel.dishes.observe(viewLifecycleOwner, Observer {
-            bestAdapter.submitList(it)
+    //add cart menu
+    override val prepareToolbar: (ToolbarBuilder.() -> Unit) = {
+        this.addMenuItem(MenuItemHolder(
+            title = "Cart",
+            menuId = R.id.action_cart,
+            icon = R.drawable.ic_cart
+        ) {
+            viewModel.navigate(NavigationCommand.To(R.id.cartDestination))
         })
+    }
 
+    //setup adapters
+    private val recommendAdapter = DishAdapter(
+        listener = {
+            val action = HomeFragmentDirections.actionHomeDestinationToDishFragment(
+                id = it.id,
+                name = it.name,
+                price = it.price.toString(),
+                oldPrise = it.oldPrice,
+                description = it.description
+            )
+            viewModel.navigate(NavigationCommand.To(action.actionId, action.arguments))
+        },
+        favoriteListener = { viewModel.handleToggleFavorite(it.id) },
+        addToCartListener = { viewModel.handleAddToCart(it.id) }
+    )
+    private val bestAdapter = DishAdapter(
+        listener = {
+            val action = HomeFragmentDirections.actionHomeDestinationToDishFragment(
+                id = it.id,
+                name = it.name,
+                price = it.price.toString(),
+                oldPrise = it.oldPrice,
+                description = it.description
+            )
+            viewModel.navigate(NavigationCommand.To(action.actionId, action.arguments))
+        },
+        favoriteListener = { viewModel.handleToggleFavorite(it.id) },
+        addToCartListener = { viewModel.handleAddToCart(it.id) }
+    )
+    private val popularAdapter = DishAdapter(
+        listener = {
+            val action = HomeFragmentDirections.actionHomeDestinationToDishFragment(
+                id = it.id,
+                name = it.name,
+                price = it.price.toString(),
+                oldPrise = it.oldPrice,
+                description = it.description
+            )
+            viewModel.navigate(NavigationCommand.To(action.actionId, action.arguments))
+        },
+        favoriteListener = { viewModel.handleToggleFavorite(it.id) },
+        addToCartListener = { viewModel.handleAddToCart(it.id) }
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun setupViews() {
-//        rvRecommend.adapter = recommendAdapter
-//        rvRecommend.addItemDecoration(HorizontalSpacingItemDecorator(recommendAdapter))
 
-        rvBest.adapter = bestAdapter
-        rvBest.layoutManager =
-            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-        rvBest.addItemDecoration(OffsetItemDecorator(left = 8))
+        //setup RecyclerViews
+        rvRecommend.setupRV(recommendAdapter)
+        rvBest.setupRV(bestAdapter)
+        rvPopular.setupRV(popularAdapter)
 
-//        rvPopular.adapter = popularAdapter
-//        rvPopular.addItemDecoration(HorizontalSpacingItemDecorator(popularAdapter))
+        tvBestSeeAll.setThrottledClickListener { viewModel.navigate(NavigationCommand.To(R.id.menuDestination)) }
+        tvPopularSeeAll.setThrottledClickListener { viewModel.navigate(NavigationCommand.To(R.id.menuDestination)) }
+        tvRecommendSeeAll.setThrottledClickListener { viewModel.navigate(NavigationCommand.To(R.id.menuDestination)) }
+
+        //setup observers
+        viewModel.recommendDishes.observe(viewLifecycleOwner) {
+            if (it.isNotEmpty()) {
+                groupRecommend.show()
+                Timber.d("Recommend dishes -> $it")
+                recommendAdapter.submitList(it)
+            }
+        }
+        viewModel.bestDishes.observe(viewLifecycleOwner) {
+            bestAdapter.submitList(it)
+        }
+
+        viewModel.popularDishes.observe(viewLifecycleOwner) {
+            popularAdapter.submitList(it)
+        }
     }
 
+    private fun RecyclerView.setupRV(adapter: DishAdapter) {
+        this.adapter = adapter
+        this.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        this.addItemDecoration(OffsetItemDecorator(left = 8))
+    }
 
 }
--- app/src/main/java/ru/skillbranch/sbdelivery/ui/fragment/home/HomeViewModel.kt
+++ app/src/main/java/ru/skillbranch/sbdelivery/ui/fragment/home/HomeViewModel.kt
@@ -1,21 +1,36 @@
 package ru.skillbranch.sbdelivery.ui.fragment.home
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.liveData
-import ru.skillbranch.sbdelivery.repository.DeliveryRepository
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import ru.skillbranch.sbdelivery.repository.DishRepository
 import ru.skillbranch.sbdelivery.ui.base.BaseViewModel
 import ru.skillbranch.sbdelivery.ui.base.IViewModelState
-import timber.log.Timber
+import ru.skillbranch.sbdelivery.ui.base.Notification
 
 class HomeViewModel(
     handle: SavedStateHandle,
-    private val repository: DeliveryRepository
+    private val dishRepository: DishRepository
 ) : BaseViewModel<HomeState>(handle, HomeState()) {
 
-    val dishes = liveData {
-        val list = repository.getDishes()
-        Timber.d("list -> $list")
-        emit(list)
+    val recommendDishes = liveData {
+        dishRepository.getRecommendDishes().collect { emit(it) }
+    }
+    val bestDishes = dishRepository.getDishesByRating(rating = 1F).asLiveData()
+    val popularDishes = dishRepository.getDishesWithMaxLikeCount().asLiveData()
+
+    fun handleToggleFavorite(dishId: String) {
+        viewModelScope.launch() {
+            dishRepository.toggleFavorite(dishId)
+        }
+    }
+
+    fun handleAddToCart(id: String, count: Int = 1) {
+        notify(Notification.TextMessage("was added to the cart"))
+        dishRepository.addToCart(id, count)
     }
 
 }
