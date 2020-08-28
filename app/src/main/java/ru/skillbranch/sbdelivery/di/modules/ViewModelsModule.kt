35212b085deede694cc0d3c00d68422257e844b3
UrucXay <urucxay@gmail.com>
2020-08-28 12:35:51 +0300

    Add new viewmodels to the module

--- app/src/main/java/ru/skillbranch/sbdelivery/di/modules/ViewModelsModule.kt
+++ app/src/main/java/ru/skillbranch/sbdelivery/di/modules/ViewModelsModule.kt
@@ -6,7 +6,10 @@ import org.koin.dsl.module
 import ru.skillbranch.sbdelivery.ui.activity.root.RootViewModel
 import ru.skillbranch.sbdelivery.ui.fragment.dish.DishViewModel
 import ru.skillbranch.sbdelivery.ui.fragment.home.HomeViewModel
+import ru.skillbranch.sbdelivery.ui.fragment.login.LoginViewModel
 import ru.skillbranch.sbdelivery.ui.fragment.menu.MenuViewModel
+import ru.skillbranch.sbdelivery.ui.fragment.register.RegisterViewModel
+import ru.skillbranch.sbdelivery.ui.fragment.restore.RestoreViewModel
 import ru.skillbranch.sbdelivery.ui.search.SearchViewModel
 
 val viewModelsModule = module {
@@ -27,13 +30,36 @@ val viewModelsModule = module {
         dishRepository = get()
     ) }
 
-    viewModel { (handle: SavedStateHandle) -> MenuViewModel(
-        handle = handle,
-        categoryRepository = get()
-    ) }
+    viewModel { (handle: SavedStateHandle) ->
+        MenuViewModel(
+            handle = handle,
+            categoryRepository = get()
+        )
+    }
 
-    viewModel { (handle: SavedStateHandle) -> SearchViewModel(
-        handle = handle
-    ) }
+    viewModel { (handle: SavedStateHandle) ->
+        SearchViewModel(
+            handle = handle
+        )
+    }
+
+    viewModel { (handle: SavedStateHandle) ->
+        LoginViewModel(
+            handle = handle
+        )
+    }
+
+    viewModel { (handle: SavedStateHandle) ->
+        RegisterViewModel(
+            handle = handle
+        )
+    }
+
+    viewModel { (handle: SavedStateHandle) ->
+        RestoreViewModel(
+            handle = handle
+        )
+    }
+
 
 }
